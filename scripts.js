12fb95c02ce82695f0397a1ac1ba10682650f93f
Mike Marlow <michael.s.marlow@gmail.com>
2017-07-26 18:04:38 -0600

    initial changes on this branch

--- scripts.js
+++ scripts.js
@@ -49,6 +49,7 @@ function addClassOfCompletedTask() {
   // if card hasClass() of '.completed-task', remove it.
   // if card does not have '.completed-task', give it one.
   currentCard.toggleClass("completed-task");
+
 }
 
 
